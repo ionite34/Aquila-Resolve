bf0d9997e18cf0825bb90acb9d0ad1e106e3e936
Ionite <dev@ionite.io>
2022-05-14 22:08:09 -0400

    Branched paths to module init

---
+++ src/Aquila_Resolve/models/__init__.py
@@ -0,0 +1,11 @@
+import sys
+
+if sys.version_info < (3, 9):
+    # In Python versions below 3.9, this is needed
+    from importlib_resources import files
+else:
+    # Since python 3.9+, importlib.resources.files is built-in
+    from importlib.resources import files
+
+
+MODELS_PATH = files(__name__)
