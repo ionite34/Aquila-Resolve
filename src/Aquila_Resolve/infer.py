fa492dcf145d9f9a14100a3fb2e60645d9627f16
Ionite <dev@ionite.io>
2022-05-15 01:16:58 -0400

    Added downloads to infer

--- src/Aquila_Resolve/infer.py
+++ src/Aquila_Resolve/infer.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from .models.dp.phonemizer import Phonemizer
 from .data import DATA_PATH
+from .data.remote import ensure_download, check_updates
 from .models import MODELS_PATH
 import sys
 
@@ -9,6 +10,8 @@ sys.path.insert(0, str(MODELS_PATH))
 
 class Infer:
     def __init__(self, device='cpu'):
+        ensure_download()  # Download checkpoint if necessary
+        check_updates()  # Check for checkpoint updates
         checkpoint_path = DATA_PATH.joinpath('model.pt')
         self.model = Phonemizer.from_checkpoint(checkpoint_path, device=device)
         self.lang = 'en_us'
